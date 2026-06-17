b5097ef21ae2f5aaf1e450f78b3c97bc34c3db35
Carlos Michell <clzwdias@gmail.com>
2026-06-17 14:03:47 -0300

    feat: bridge tauri-api com re-exports

--- src/lib/tauri-api.js
+++ src/lib/tauri-api.js
@@ -1,3 +1,5 @@
+export { selectPdfFile, selectSaveLocation } from './dialogs.js';
+export { readBinaryFile, writeBinaryFile, readTextFile } from './file-io.js';
 export function isTauri() {
   return typeof window !== 'undefined' && '__TAURI_INTERNALS__' in window;
 }
